a90ffadba167276a2f0417557df2246e38359cf1
Dia Brar <diakb42@gmail.com>
2025-12-07 20:44:45 -0500

    quizzes default empty

--- Kambaz/Database/quizzes.js
+++ Kambaz/Database/quizzes.js
@@ -1,12 +1,1 @@
-export default [
-  {
-    _id: "01",
-    title: "test",
-    course: "RS101",
-    quizType: "Graded quiz",
-    points: 100,
-    assignmentGroup: "Quizzes",
-    shuffleAnswers: "Yes",
-    timeLimit: 20,
-  },
-];
+export default [];
